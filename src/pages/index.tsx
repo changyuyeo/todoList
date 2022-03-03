09087d15d6e74a30c00de6b830ef596a5133cd84
changyuyeo <wry5533@gmail.com>
2022-03-03 19:39:11 +0900

    project setting

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from 'next'
+import styled from 'styled-components'
+
+const Container = styled.div`
+	font-style: italic;
+`
+
+const HomePage: NextPage = () => {
+	return <Container>HomePage</Container>
+}
+
+export default HomePage
